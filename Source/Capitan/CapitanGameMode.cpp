b6c322da1986751c5d173fee2e54fc966d4d3599
bikekun <potoshop1@gmail.com>
2016-07-04 22:19:46 +0300

    Настройка корабля

--- Source/Capitan/CapitanGameMode.cpp
+++ Source/Capitan/CapitanGameMode.cpp
@@ -7,7 +7,7 @@
 ACapitanGameMode::ACapitanGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Geometry/Meshes/fbx/botLuck/korpus/NewBlueprint"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
